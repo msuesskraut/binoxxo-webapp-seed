1e279e4859f21eef5f03f8c02667a55bb9a87733
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2018-12-25 11:04:26 +0100

    backport python server to python2

--- serve.py
+++ serve.py
@@ -2,6 +2,9 @@
 
 # From https://gist.github.com/prideout/09af26cef84eef3e06a1e3f20a499a48
 
+from __future__ import print_function
+from contextlib import contextmanager
+
 import http.server
 import socketserver
 
@@ -13,7 +16,16 @@ handler.extensions_map.update({
 })
 
 socketserver.TCPServer.allow_reuse_address = True
-with socketserver.TCPServer(("", PORT), handler) as httpd:
+
+@contextmanager
+def CreateTCPServer(addr, handler):
+    httpd = socketserver.TCPServer(("", PORT), handler)
+    try:
+        yield httpd
+    finally:
+        httpd.server_close()
+
+with CreateTCPServer(("", PORT), handler) as httpd:
     httpd.allow_reuse_address = True
     print("Serving at port", PORT)
     httpd.serve_forever()
