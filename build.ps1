3e47209deb1d4232ed7f8fccee5d7b5e2cab0b07
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2018-12-25 11:18:28 +0100

    customize for binoxxo

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 cargo build --target wasm32-unknown-unknown
-wasm-bindgen target/wasm32-unknown-unknown/debug/appname.wasm --no-modules --out-dir ./pkg
+wasm-bindgen target/wasm32-unknown-unknown/debug/binoxxo_webapp_seed.wasm --no-modules --out-dir ./pkg
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cargo build --target wasm32-unknown-unknown
-wasm-bindgen target/wasm32-unknown-unknown/debug/appname.wasm --no-modules --out-dir ./pkg
+wasm-bindgen target/wasm32-unknown-unknown/debug/binoxxo_webapp_seed.wasm --no-modules --out-dir ./pkg
