7f5df8847c1e21f9f65e176de4eb211a18214f4e
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2020-05-08 23:39:14 +0200

    ported to seed 0.7.0

--- src/lib.rs
+++ src/lib.rs
@@ -4,41 +4,15 @@ mod model;
 mod view;
 
 use crate::control::{update, Message, DIFFICULTY_STORAGE, HELPER_STORAGE, LANGUAGE_STORAGE};
-use crate::model::{Difficulty, Helper, Language, Model};
+use crate::model::Model;
 use crate::view::view;
 use seed::prelude::*;
-
-fn load_difficulty() -> Option<Difficulty> {
-    if let Some(storage) = seed::storage::get_storage() {
-        if let Ok(Some(loaded_serialized)) = storage.get_item(DIFFICULTY_STORAGE) {
-            return serde_json::from_str(&loaded_serialized).ok();
-        }
-    }
-    None
-}
-
-fn load_language() -> Option<Language> {
-    if let Some(storage) = seed::storage::get_storage() {
-        if let Ok(Some(loaded_serialized)) = storage.get_item(LANGUAGE_STORAGE) {
-            return serde_json::from_str(&loaded_serialized).ok();
-        }
-    }
-    None
-}
-
-fn load_helper() -> Option<Helper> {
-    if let Some(storage) = seed::storage::get_storage() {
-        if let Ok(Some(loaded_serialized)) = storage.get_item(HELPER_STORAGE) {
-            return serde_json::from_str(&loaded_serialized).ok();
-        }
-    }
-    None
-}
+use seed::browser::web_storage::LocalStorage;
 
 fn after_mount(_url: Url, _orders: &mut impl Orders<Message>) -> AfterMount<Model> {
-    let difficulty = load_difficulty().unwrap_or_default();
-    let language = load_language().unwrap_or_default();
-    let helper = load_helper().unwrap_or_default();
+    let difficulty = LocalStorage::get(DIFFICULTY_STORAGE).unwrap_or_default();
+    let language = LocalStorage::get(LANGUAGE_STORAGE).unwrap_or_default();
+    let helper = LocalStorage::get(HELPER_STORAGE).unwrap_or_default();
     let model = Model::new(difficulty, helper, language);
     AfterMount::new(model)
 }
